685cff96a649383c208db984ffaeb483af3c8bf8
Alexander Berntsen <alexander@plaimi.net>
2014-07-25 14:31:36 +0200

    Add showL8 for converting showable to L.ByteString
    Signed-off-by: Alexander Berntsen <alexander@plaimi.net>
    Signed-off-by: Stian Ellingsen <stian@plaimi.net>
    Reviewed-by: Stian Ellingsen <stian@plaimi.net>

---
+++ src/Text.hs
@@ -0,0 +1,12 @@
+{- |
+Module      :  $Header$
+Description :  Text manipulation.
+Copyright   :  (c) plaimi 2014
+License     :  GPL-3
+
+Maintainer  :  plailude@plaimi.net
+-} module Text (
+  showL8
+  ) where
+
+import Text.Show
---
+++ src/Text/Show.hs
@@ -0,0 +1,15 @@
+{- |
+Module      :  $Header$
+Description :  Show various Text formats.
+Copyright   :  (c) plaimi 2014
+License     :  GPL-3
+
+Maintainer  :  plailude@plaimi.net
+-} module Text.Show where
+
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L8
+
+showL8 :: Show a => a -> L.ByteString
+-- | 'showL8' converts a showable value to a 'L.ByteString' using 'show'.
+showL8 = L8.pack . show
